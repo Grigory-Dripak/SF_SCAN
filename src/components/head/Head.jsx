8895a164f6af2cb6ba0d7d42cb7d00668cddc57d
Grigory-Dripak <126253741+Grigory-Dripak@users.noreply.github.com>
2024-09-07 00:44:51 +0300

    header with user Ava

--- src/components/head/Head.jsx
+++ src/components/head/Head.jsx
@@ -2,14 +2,21 @@ import React from 'react'
 import styles from './Head.module.css'
 import { Link, useNavigate } from "react-router-dom";
 import { useSelector } from 'react-redux';
+import { useActions } from '../../hooks/useActions';
 
 export default function Header() {
   const {user} = useSelector(state => state)
   const navigate = useNavigate();
+  const {removeUser} = useActions()
 
   const handleLoginClick = () => {
     return navigate('/login');
   };
+
+  const handleQuitClick = () => {
+    removeUser()
+    return false
+  }
 
   return (
     <header className={styles.header}>
@@ -21,7 +28,11 @@ export default function Header() {
         </div>
         {user.isAuth && (<div className={styles.user_pannel}>0</div>)}
         {user.isAuth ? 
-        (<div className={styles.user_data}>1</div>) 
+        (<div className={styles.user_data}>
+          <div className={styles.name}>Алексей А.</div>
+          <div className={styles.ava}></div>
+          <a href="#" className={styles.quit} onClick={handleQuitClick}>Выйти</a>
+        </div>) 
         : 
         (<div className={styles.get_auth}>
           <Link to="/login">Зарегистрироваться</Link>
--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useForm } from 'react-hook-form'
 import styles from './Login.module.css'
 import { useActions } from '../../hooks/useActions';
